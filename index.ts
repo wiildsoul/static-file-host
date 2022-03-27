a6141a722ac64e8bf3eb305f233ab7c1c19a4111
abishek5724 <abishekabi5724@gmail.com>
2022-03-27 15:29:07 +0530

    final touches

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 import * as multer from "multer";
 import * as cors from "cors";
-import { gltfFilter, storage } from "./utils";
+import * as path from "path";
+import { gltfFilter, storage, listDirs, pageMaker, linkMaker } from "./utils";
 
 // setup
-const UPLOAD_PATH = "uploads";
+export const UPLOAD_PATH = "uploads";
 const upload = multer({
   dest: `${UPLOAD_PATH}/`,
   fileFilter: gltfFilter,
@@ -17,9 +18,22 @@ const app = express();
 app.use(cors());
 app.use(express.static("uploads/"));
 
+app.get("/", (req, res, next) => {
+  let x = "";
+  let dirs = listDirs(path.join(__dirname, UPLOAD_PATH));
+  for (let dir of dirs) {
+    console.log(dir)
+    x += linkMaker(dir, `/${dir}`);
+  }
+  res.send(pageMaker("<h2>All files uploaded till now.</h2>" + "<ul>" + x + "</ul>"));
+});
+
 app.post("/upload", upload.single("file"), (req: any, res, next) => {
   console.log(req.file);
   res.send(req.id);
+});
+app.get("/upload", (req, res, next) => {
+  res.send(pageMaker("<h1>upload using POST , GET not supported.</h1>"));
 });
 
 app.listen(PORT, function () {
--- utils.ts
+++ utils.ts
@@ -1,6 +1,6 @@
 import { diskStorage } from "multer";
 import { join, extname } from "path";
-import { mkdir } from "fs";
+import { mkdir, readdirSync } from "fs";
 import { v4 as uuid4 } from "uuid";
 
 
@@ -9,6 +9,10 @@ export function gltfFilter(req, file, cb) {
     return cb(new Error("Only GLTF files are allowed!"), false);
   }
   cb(null, true);
+}
+
+export const listDirs = (dirname)=> {
+  return readdirSync(dirname)
 }
 
 export const storage = diskStorage({
@@ -23,3 +27,29 @@ export const storage = diskStorage({
   },
 });
 
+
+export function pageMaker(body: String) {
+  return  `<html>
+  <head>
+    <title> Major Project</title>
+    <style>
+    body {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+      height:100vh;
+      width:100vw;
+      display flex;
+      justify-content: center;
+      align-items: center;
+    }
+    </style>
+  </head>
+  <body>
+    ${body}
+  </body>
+  </html>
+`
+}
+
+export function linkMaker(text, link) {
+  return `<li><a href="${link}">${text}</a></li>`
+}
